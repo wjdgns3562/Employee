60ac930856403137fa01daf3e8172fb03176709a
wjdgns3562 <wjdgns3562@nate.com>
2015-11-09 17:22:57 +0900

    dd

--- src/test/resources/sql/world.sql
+++ src/test/resources/sql/world.sql
@@ -1,14 +1,32 @@
-select * from city;
+
 
 select count(*) 
 
 drop table member;
 
-create table member(
-	id 				numeric,
+create table member (
+	id 				int auto_increment,
 	email			varchar(255),
 	name			varchar(255),
 	password 		varchar(255),
-	regiter_date	date
-	constraint pk_id primary key(id)
+	regiter_date	date,
+	constraint pk_id primary key (id)
 );
+
+--
+--Member CRUD(Create, Read, Update, Delete)
+--
+--Create
+insert into member
+(email, name, password, regiter_date)
+values
+('xxx@yyy','홍길동','1234','2015/11/11')
+
+--Read 
+select * from member;
+
+--Update
+update member set name= '홍순이2', password ='9876' where id=5;
+
+--Delete
+delete from member;
